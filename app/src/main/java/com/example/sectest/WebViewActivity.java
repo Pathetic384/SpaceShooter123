d305f3316b5dc6422119491cb0ab222111127e53
ICEcream2714 <22028087@vnu.edu.vn>
2024-03-29 23:21:31 +0700

    bruh

--- app/src/main/java/com/example/sectest/WebViewActivity.java
+++ app/src/main/java/com/example/sectest/WebViewActivity.java
@@ -3,15 +3,21 @@ package com.example.sectest;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Button;
 
 public class WebViewActivity extends AppCompatActivity {
-
+    Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        backBtn = findViewById(R.id.btn_back);
+        backBtn.setOnClickListener(v -> {
+            // Go back to the main activity
+            finish();
+        });
         WebView simpleWebView = findViewById(R.id.web_view_xl);
-        simpleWebView.loadUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        simpleWebView.loadUrl("https://www.wikihow.com/Play-Games-Well");
     }
 }
